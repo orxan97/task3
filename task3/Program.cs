7bc25f22234059ce5beae787239862417ad98e68
orxan97 <orkhanna@code.edu.az>
2022-11-29 17:43:54 +0400

    Add project files.

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student
+            {
+              Name ="Orxan",
+              Surname = "Aslanov",
+              Age=25,
+              Group ="P327",
+              
+
+            
+            };
+
+            Console.WriteLine(student.GetStudentFullName());
+            student.GetStudent();
+            student.GetGrade();
+        }
+        
+    }
+}
---
+++ task3/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+   
+        class Student
+        {
+            public string Name;
+            public string Surname;
+            public int Age;
+            public string Group;
+            public int Point;
+            public bool IsGraduated;
+
+            public string GetStudentFullName()
+            {
+                string fullname = Name + " " + Surname;
+                return fullname;
+            }
+
+            public void GetStudent()
+            {
+            Console.WriteLine("Mezun olub olmadiginizi daxil edin");
+            IsGraduated = Convert.ToBoolean(Console.ReadLine());
+                if (IsGraduated==true)
+                {
+                    Console.WriteLine(Name + " " + Surname + " " + "Group:" + Group + " " + "Age:" + Age + " " + "Point:" + Point);
+                    Console.WriteLine("Telebe mezun olub");
+                }
+                else if(IsGraduated==false)
+                {
+                    Console.WriteLine("Telebe mezun olmayib");
+                }
+
+            }
+
+            public void GetGrade()
+            {
+            Console.WriteLine("Balinizi daxil edin");
+            Point =Convert.ToInt32( Console.ReadLine());
+                if (Point > 80)
+                {
+                    Console.WriteLine("Telebe imtahana daxil ola biler");
+                }
+                else
+                {
+                    Console.WriteLine("Telebe imtahana daxil ola bilmez");
+                }
+            }
+
+        }
+    }
+
